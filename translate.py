2829e5170b2b311d405ee49640697850b90f1016
Reddi Nithin <99125721+nithin17298@users.noreply.github.com>
2024-12-26 12:27:44 +0530

    Create translate.py

---
+++ translate.py
@@ -0,0 +1,8 @@
+from googletrans import Translator
+
+translator=Translator()
+
+def translate_text(original_text:str)->str:
+   translation = translator.translate(original_text,dest='en')
+   translated_text,original_language=translation.text,translation.src
+   return translated_text,original_language
