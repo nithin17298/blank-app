c485305d70c457326e04652d3a7b6b2db964a44a
Reddi Nithin <99125721+nithin17298@users.noreply.github.com>
2025-01-02 09:42:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,6 +82,7 @@ if input_option == "Text":
         if st.button("Transcribe"):
             if text_input:
                 st.write("Transcribed Text:")
+                st.write(text_input)
                 #st.write(preprocess(text_input)) 
                 label=predict_sentiment(text_input)
                 st.write("Detection:") 
